304bc406cc418fec89a98ae7c9c06069ef2dadcc
Fábio Lucena Ribas <fabio.luce@outlook.com>
2019-02-18 21:40:37 -0300

    Ajustes

--- ServidorConsultorio/IService1.cs
+++ ServidorConsultorio/IService1.cs
@@ -2,9 +2,8 @@
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using Biblioteca.ClassesBasicas;
-using static Biblioteca.Dados.DadosBuscaCEP;
 
-namespace ServidorPetSpa
+namespace ServidorConsultorio
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
